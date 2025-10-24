0b1e547cda58362bf7b3eb00fb29d4655e3fe441
corecher <wooverjin@gmail.com>
2025-10-24 11:24:32 +0900

    UI change/ WaveManager

---
+++ Assets/Script/Wave/WaveManager.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    public List<GameObject> enemy = new List<GameObject>();
+    public int waveCount;
+    void Update()
+    {
+        
+    }
+}
