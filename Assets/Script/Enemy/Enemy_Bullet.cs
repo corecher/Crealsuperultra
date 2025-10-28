ab1ec6d7da95fe0af2d65c17b29bc8c789c5914d
corecher <wooverjin@gmail.com>
2025-10-28 22:05:24 +0900

    LevelUpUI

--- Assets/Script/Enemy/Enemy_Bullet.cs
+++ Assets/Script/Enemy/Enemy_Bullet.cs
@@ -6,10 +6,12 @@ public class Bullet : MonoBehaviour
     [SerializeField] float speed;
     [SerializeField] float destroyTime;
     [SerializeField] int wave; //���� �������� GameManager.instance.wave�� ��ü
-
+    [SerializeField]private Player_Controller player_Controller;
 
     void Start()
     {
+        GameObject player = GameObject.Find("Player");
+        player_Controller = GetComponent<Player_Controller>();
         Destroy(gameObject, destroyTime);
     }
 
@@ -23,7 +25,7 @@ public class Bullet : MonoBehaviour
         if (player.CompareTag("Player"))
         {
             Debug.Log(" ÷  ̾  hp -1     ");
-            //player.GetDamage((wave >= 10) ? 2 : 1);
+            player_Controller.GetDamage((wave >= 10) ? 2 : 1);
         }
     }
 }
